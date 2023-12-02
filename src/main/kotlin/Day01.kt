05d045ba4e2d56edb6c6923fe824cd33d6fb216b
vlepalud <valentin.lepalud@gmal.com>
2023-12-02 16:42:30 +0100

    day 1 part 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,7 +1,40 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    readFilePart1(fileName = "src/main/resources/day01.txt")
+}
+fun readFilePart1(fileName: String){
+    var sum=0;
+    File(fileName).forEachLine { it ->
+        val (digits,_) = it.partition { it.isDigit() }
+        println(it+" "+ digits+" "+digits[0]+digits[digits.length-1]);
+        sum+=(digits[0].digitToInt()*10+digits[digits.length-1].digitToInt());
+    }
+    println(sum);
+}
+fun readFilePart2(fileName: String){
+    var sum=0;
+
+    File(fileName).forEachLine { it ->
+        val (digits,_) = it.partition { it.isDigit() }
+
+    }
+}
+
+fun hasADigit(word: String){
+    val StringToInt=HashMap<String,Int>()
+    StringToInt["one"]=1;
+    StringToInt["two"]=2;
+    StringToInt["three"]=3;
+    StringToInt["four"]=4;
+    StringToInt["five"]=5;
+    StringToInt["six"]=6;
+    StringToInt["seven"]=7;
+    StringToInt["eigth"]=8;
+    StringToInt["nine"]=9;
+wo
+    StringToInt.forEach(
+
+
+    )
 }
