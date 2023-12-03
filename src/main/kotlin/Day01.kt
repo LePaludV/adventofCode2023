010f6e17740eb53a2714a769dbb8d7468d0127ca
Valentin Le Palud <valentin.lepalud@gmail.com>
2023-12-03 19:17:25 +0100

    day 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -2,26 +2,19 @@ import java.io.File
 
 fun main(args: Array<String>) {
     readFilePart1(fileName = "src/main/resources/day01.txt")
+    readFilePart2(fileName = "src/main/resources/day01.txt")
 }
 fun readFilePart1(fileName: String){
     var sum=0;
     File(fileName).forEachLine { it ->
         val (digits,_) = it.partition { it.isDigit() }
-        println(it+" "+ digits+" "+digits[0]+digits[digits.length-1]);
+        //println(it+" "+ digits+" "+digits[0]+digits[digits.length-1]);
         sum+=(digits[0].digitToInt()*10+digits[digits.length-1].digitToInt());
     }
     println(sum);
 }
 fun readFilePart2(fileName: String){
     var sum=0;
-
-    File(fileName).forEachLine { it ->
-        val (digits,_) = it.partition { it.isDigit() }
-
-    }
-}
-
-fun hasADigit(word: String){
     val StringToInt=HashMap<String,Int>()
     StringToInt["one"]=1;
     StringToInt["two"]=2;
@@ -30,11 +23,29 @@ fun hasADigit(word: String){
     StringToInt["five"]=5;
     StringToInt["six"]=6;
     StringToInt["seven"]=7;
-    StringToInt["eigth"]=8;
+    StringToInt["eight"]=8;
     StringToInt["nine"]=9;
-wo
-    StringToInt.forEach(
+    val regex = Regex("(?=((\\d)|one|two|three|four|five|six|seven|eight|nine))")
+
+    File(fileName).forEachLine {
+        val matches = regex.findAll(it)
+        val digits = mutableListOf<String>()
+        for (match in matches) {
+            val num=match.groupValues[1]
+            if(StringToInt.containsKey(num)){
+                digits.add(StringToInt[num].toString())
+            }
+            else{
+                digits.add(num)
+            }
+        }
+
+        val firstDigit = digits[0]
+        val lastDigit = digits.last()
+        sum+=firstDigit.toInt()*10+lastDigit.toInt()
+
+    }
+    println(sum)
 
 
-    )
 }
